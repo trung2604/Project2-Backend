2170d3d80b7b35cb7d4915627b44bec73c4e93d3
Do Dinh Trung <161141020+trung2604@users.noreply.github.com>
2025-05-13 17:49:00 +0700

    Initial commit: BookStore project setup

---
+++ src/test/java/com/project2/BookStore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project2.BookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
